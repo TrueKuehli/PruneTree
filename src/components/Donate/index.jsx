5f8710580e1227b564ef8f7a812ae0994d5d31b3
Tobias Gray <tobias.gray.dev@gmail.com>
2023-06-08 17:40:54 +0100

    Add patreon donate

--- src/components/Donate/index.jsx
+++ src/components/Donate/index.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import axios from 'axios'
-import paypal from './paypal.png'
+import DonateTarget from '../Home/DonateTarget'
+import SiteStats from '../Home/SiteStats'
 
 export default () => {
   const [bill, setBill] = useState(0)
   const [donations, setDonations] = useState(0)
+  const myRef = useRef(null)
 
   useEffect(() => {
+    myRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
     axios.get('/api/bills')
       .then((response) => {
         const { billCents, donationsCents } = response.data
@@ -28,22 +31,29 @@ export default () => {
   }
 
   return (
-    <div className='container'>
-      <h1>Donate</h1>
-      <p>It has been fantastic creating a tool that so many people love and use to track their Sims legacies. However with this rise in popularity so do the monthly costs.</p>
-      <p>The Plum Tree is a personal project that is free to use, so not a great business model.</p>
-      <p>Every donation goes a little way towards the monthly bills I have to pay. I humbly thank those of you that are able to contribute even just a little.</p>
-      <a
-        className='btn btn-lg btn-primary'
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZXUTDCZXY7L28&source=url'
-      >
-        Donate using <img src={paypal} height='30' style={{ verticalAlign: 'middle' }} />
-      </a>
+    <div className='container' ref={myRef}>
+      <h1>Why Donate?</h1>
+      <p>Hi my name is Tobias.</p>
+      <p>It has been fantastic creating a tool that so many people love and use
+        to track their Sims legacies. However with this rise in popularity so do
+        the monthly costs.
+      </p>
+      <p>The Plum Tree started out as personal project free to use for what I
+        expected to only be a small number of users. Every donation goes a
+        little way towards the monthly bills of storing terabytes of uploaded
+        images, thousands of trees and handling the vast number of user traffic
+        browsing the trees created.
+      </p>
+      <p>I humbly thank those of you that are able to contribute even just a little.</p>
+      <DonateTarget />
+      <SiteStats />
       <h2>Last Months Costs</h2>
       <p>Last months bill was {formatCents(bill)}, donations this month are up to {formatCents(donations)} which means we've covered {getPercent()} of that.</p>
-      <p>Note the the stats above may take up to 24 hours to update and the donation total is that after currency conversions with fees deducted.</p>
+      <div className='alert alert-info'>
+        <div className='alert-body'>
+          Note the the stats above may take up to 24 hours to update and the donation total is that after currency conversions with fees deducted.
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Home/SiteStats/index.jsx
+++ src/components/Home/SiteStats/index.jsx
@@ -3,25 +3,25 @@ import React from 'react'
 export default () => {
   return (
     <div>
-      <h2>Some Stats We're Proud Of</h2>
-      <p>Looks like our user have been busy creating trees so we thought we should share some stats so you can see just how busy our users have been.</p>
+      <h2>Some Big Numbers</h2>
+      <p>Looks like our user have been busy creating trees so I thought I would share some stats so you can see just how busy you've been.</p>
       <table className='table' style={{ maxWidth: 500, margin: '20px auto' }}>
         <tbody>
           <tr>
             <th>Users Signed Up</th>
-            <td>120,000+</td>
+            <td>220,000+</td>
           </tr>
           <tr>
             <th>Trees Created</th>
-            <td>250,000+</td>
+            <td>500,000+</td>
           </tr>
           <tr>
             <th>Trees Published</th>
-            <td>40,000+</td>
+            <td>70,000+</td>
           </tr>
           <tr>
             <th>Images Uploaded</th>
-            <td>2,000,000+</td>
+            <td>8,000,000+<br />(1.8 terabytes)</td>
           </tr>
         </tbody>
       </table>
