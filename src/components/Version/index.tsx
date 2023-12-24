bdf618d412db1b2fd658cf2cc92c4de533149855
Timon Scholz <t.scholz@tu-braunschweig.de>
2023-12-24 01:35:59 +0100

    Updated Version page

--- src/components/Version/index.tsx
+++ src/components/Version/index.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
 
 import styles from './styles.scss';
+import {Link} from 'react-router-dom';
 
 
 /**
  * Component to display the full details of the current app version.
  */
 export default function Version() {
+  // Transform the git repo url to a url that can be used to open the repo in the browser.
+  const repoUrl = (GIT_REPO_URL.startsWith('git@') ?
+      GIT_REPO_URL.replace(':', '/').replace('git@', 'https://') :
+      GIT_REPO_URL
+  ).replace('.git', '');
+
+  const versionUrl = `${repoUrl}/releases/tag/${PACKAGE_VERSION}`;
+  const commitUrl = `${repoUrl}/commit/${COMMIT_HASH}`;
+  const shortCommitHash = COMMIT_HASH.substring(0, 7);
+
   return (
     <div className={styles.panel}>
       <table>
         <tbody>
           <tr>
             <th>App Version</th>
-            <td>{PACKAGE_VERSION}</td>
+            <td><Link to={versionUrl} target='_blank' rel='noopener noreferrer'>{PACKAGE_VERSION}</Link></td>
           </tr>
           <tr>
             <th>Last Commit</th>
-            <td>{COMMIT_HASH.substring(0, 7)}</td>
+            <td><Link to={commitUrl} target='_blank' rel='noopener noreferrer'>{shortCommitHash}</Link></td>
           </tr>
           <tr>
             <th>Build Date</th>
-            <td>{BUILD_DATE}</td>
+            <td>{(new Date(BUILD_DATE)).toLocaleString()}</td>
           </tr>
         </tbody>
       </table>
