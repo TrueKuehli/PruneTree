28125e76d399fa2a532b5c584d2598ff8c63e032
Jakub Cabala <kuba.cabala6@gmail.com>
2024-02-12 20:02:36 +0000

    style(video-player): Fixed styles issues in VideoPlayer component

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,32 +1,36 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, {useState, useLayoutEffect} from 'react';
 
-import styles from "./styles.scss";
+import styles from './styles.scss';
 
 type Props = {
   src: string;
 };
 
-export default function VideoPlayer({ src }: Props) {
+/**
+ * Player for videos with placeholder
+ * @param src Source for the video
+ */
+export default function VideoPlayer({src}: Props) {
   const [showVideo, setShowVideo] = useState(false);
 
   useLayoutEffect(() => {
-    const videoPlayerAllowed = localStorage.getItem("allow-video-player")
-    if (videoPlayerAllowed === "true") {
-      allowVideoPlayer()
+    const videoPlayerAllowed = localStorage.getItem('allow-video-player');
+    if (videoPlayerAllowed === 'true') {
+      allowVideoPlayer();
     }
-  }, [])
+  }, []);
 
   const allowVideoPlayer = () => {
-    localStorage.setItem("allow-video-player", "true")
-    setShowVideo(true)
-  }
+    localStorage.setItem('allow-video-player', 'true');
+    setShowVideo(true);
+  };
 
   return (
     <div className={styles.videoPlayerWrapper}>
       {showVideo ? (
         <iframe src={src} className={styles.videoPlayer} allowFullScreen />
       ) : (
-        <div className={`${styles.videoPlayer} ${styles.videoPlayerPlaceholder}`}> 
+        <div className={`${styles.videoPlayer} ${styles.videoPlayerPlaceholder}`}>
           <button className='btn btn-primary' onClick={allowVideoPlayer}>Show the video</button>
         </div>
       )}
